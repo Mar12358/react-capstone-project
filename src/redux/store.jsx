d3215d501d0ede563b14132a5bf8b6deb2b967a2
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-30 00:11:39 -0300

    Delete unused code

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import assetsSlice from './assets/assetsSlice';
-import ratesSlice from './rates/ratesSlice';
 import symbolSlice from './symbol/symbolSlice';
 
 const store = configureStore({
   reducer: {
     assets: assetsSlice,
-    rates: ratesSlice,
     symbol: symbolSlice,
   },
 });
